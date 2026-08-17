c6f5f9a0f770d6116b799591ccd490a34744b67a
justYu2001 <jack4781012@gmail.com>
2026-08-18 07:35:30 +0800

    chore: update next js config to pin worktree root

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  outputFileTracingRoot: import.meta.dirname,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
